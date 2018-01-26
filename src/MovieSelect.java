1a798eb51d5bcb22b0b63648110552b2f563a519
Vishesh <visheshsingh16@gmail.com>
2018-01-25 21:25:49 -0600

    Movie Selection program: Added variables for name, seats, and movies

--- src/MovieSelect.java
+++ src/MovieSelect.java
@@ -6,8 +6,8 @@ public class MovieSelect {
 	// Select number of seats
 	// print everything
 	Scanner sc = new Scanner(System.in);
+	String[] movieList = { "1) Avengers", "2) Star Wars", "3) Coco", "4) The Foreigner" };
 	String name;
-	int seats;
 
 	public void getName() {
 		System.out.print("Please enter your name: ");
@@ -18,12 +18,14 @@ public class MovieSelect {
 
 	public void getMovie() {
 		System.out.print("Which movie would you prefer watching?: ");
+		int number;
 		getSeats();
 	}
 
 	public void getSeats() {
 		System.out.print("How many tickets: ");
-		seats = sc.nextInt();
+		String seats;
+		seats = sc.nextLine();
 	}
 
 	public static void main(String[] args) {
